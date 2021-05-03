5a8dbee520bc91297c24033c64d939eb65093b50
Andreas Buneci <andreasbuneci@seattleacademy.org>
2021-05-03 11:16:15 -0700

    Add drive feature

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 1504
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/drive', (req, res) => {
+  res.send("I'm Driving");
 })
 
 app.listen(port, () => {
